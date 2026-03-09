9ee308a166e03b8fdec5344f82922607c9f724f1
yakisomi <your.yyaki@gmail.com>
2026-03-09 14:29:21 +0300

    Добавлен новый класс: TaskWithPriority

---
+++ TaskWithPriorityApp/TaskWithPriorityApp/TaskWithPriority.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskWithPriorityApp
+{
+    public enum Priority
+    {
+        Низкий,
+        Средний,
+        Высокий
+    }
+
+    public class TaskWithPriority
+    {
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public TaskWithPriority(string description, Priority priority, DateTime deadline)
+        {
+            Description = description;
+            Priority = priority;
+            IsCompleted = false;
+            Deadline = deadline;
+        }
+    }
+}
